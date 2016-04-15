1e401a2ac3478b19915607a15c1908fff2335439
tharvik <tharvik@users.noreply.github.com>
2016-04-15 16:55:04 +0200

    *: use emerge-accept

--- test.sh
+++ test.sh
@@ -8,17 +8,4 @@ cd "/overlay/${PACKAGE}"
 
 repoman full
 
-if ! emerge -p "${PACKAGE}"
-then
-	emerge --autounmask-write "${PACKAGE}" || :
-
-	find '/etc/portage' -name '._cfg*' | while read l
-	do
-		dirname="$(dirname "${l}")"
-		filename="$(basename "${l}" | cut -d _ -f 3-)"
-
-		mv "${l}" "${dirname}/${filename}"
-	done
-fi
-
-emerge "${PACKAGE}"
+emerge-accept "${PACKAGE}"
